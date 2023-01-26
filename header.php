e9886ec88aa94c5f774ed22c3c1e82805a00689b
Taylor Reid <taylorsreid@gmail.com>
2023-01-26 01:19:32 -1000

    Registration API now returns a JWT.  Split backend and frontend.

--- header.php
+++
@@ -1,7 +0,0 @@
-<table id="menubar">
-    <tr>
-        <td><a href="/">Home</a></td>
-        <td>My Tips</td>
-        <td>About</td>
-    </tr>
-</table>
--- script.js
+++
@@ -1,7 +0,0 @@
-//returns the current root of the API
-//for now it's local but this ensures
-function getAPIRoot(){
-
-    return "./api"
-
-}
