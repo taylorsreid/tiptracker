ba7e89d799ec18cc44cb07dd73c7105275375baf
Taylor Reid <taylorsreid@gmail.com>
2023-09-05 19:37:23 -1000

    sync between computers only

--- app/Http/Controllers/ShiftController.php
+++ app/Http/Controllers/ShiftController.php
@@ -17,14 +17,6 @@ class ShiftController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(StoreShiftRequest $request)
@@ -36,14 +28,6 @@ class ShiftController extends Controller
      * Display the specified resource.
      */
     public function show(Shift $shift)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Shift $shift)
     {
         //
     }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
 use App\Models\User;
-use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
@@ -16,17 +17,9 @@ class UserController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreUserRequest $request)
     {
         // controlled by app\Actions\Fortify\CreateNewUser
     }
@@ -40,17 +33,9 @@ class UserController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(User $user)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, User $user)
+    public function update(UpdateUserRequest $request, User $user)
     {
         //
     }
